1ef70b91b33fc5143c945f7150f4a8d3fb098ae8
Marco Wong <marco@Marcos-Air.frontierlocal.net>
2020-01-15 23:37:56 -0500

    First commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,74 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import logo from './logo.svg';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: 'marco-wong-1',
+      lists: [],
+    }
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  componentDidMount(){
+    this.fetchRepo();
+  }
+
+  fetchRepo() {
+    fetch(`https://api.github.com/users/${this.state.username}/repos`)
+    .then(response => response.json())
+    .then(result => result.map(repo => (
+      {
+        repoName: `${repo.name}`
+      }
+    )))
+    .then(repoN => this.setState({
+      lists: repoN // set the lists to the fetched list of repo names
+    }))
+    .catch(error => this.setState({
+      lists: [] // set the lists to empty if there is an error fetching
+    }))
+  }
+
+  handleChange(event) {
+    this.setState({
+      username: event.target.value
+    })
+  }
+  
+  handleSubmit(event) {
+    event.preventDefault();
+    this.fetchRepo();
+  }
+
+  render() {
+    return(
+      <div className="App">
+        <div className="header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <form onSubmit={this.handleSubmit}>
+            <label>
+              Username:
+              <input type="text" value={this.state.username} onChange={this.handleChange} />
+            </label>
+          </form>
+        </div>
+        <div className="listing">
+          { 
+            this.state.lists.length > 0 ? 
+            this.state.lists.map(name => {
+              return <p key={name.repoName}>{name.repoName}</p>
+            }) :
+            <h1> Found No Repository </h1>
+          }
+        </div>
+      </div>
+    )
+  }
 }
 
 export default App;
